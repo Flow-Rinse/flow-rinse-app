549739d16535ad77a98fc989ccd5440cc1b6077a
bonlemueldc <bon.delacruz@dottystylecreative.com>
2020-06-05 02:27:44 +0800

    Added Range Slider

--- src/views/page-components/RangeSlider.js
+++ src/views/page-components/RangeSlider.js
@@ -1,7 +1,8 @@
 import React from "react";
 
 // plugin that creates slider
-import Slider from "nouislider";
+// import Slider from "nouislider";
+import{ RangeSlider as RangeSliderss} from 'reactrangeslider';
 
 // reactstrap components
 import {
@@ -14,30 +15,29 @@ import {
 // core components
 
 function RangeSlider() {
+
+  const [volumeRange, setVolume] = React.useState(25);
+  
   React.useEffect(() => {
-    if (
-      !document
-        .getElementById("sliderRegular")
-        .classList.contains("noUi-target")
-    ) {
-      Slider.create(document.getElementById("sliderRegular"), {
-        start: [25],
-        connect: [true, false],
-        step: 5,
-        range: { min: 0, max: 100 }
-      });
-    }
+    
   });
+
+  function sliderOnChange(value){
+    setVolume(value)
+  }
+
   return (
     <>
       <div className="section-range-slider">
         <Container>
-          <Row>
-            <Col lg="3" sm="6">
-              <p className="category">Within 50 km. of Cavite</p>
-              <div className="slider" id="sliderRegular"></div>
-            </Col>
-          </Row>
+        <p className="category">{"Within "+volumeRange+" km. of Cavite"}</p>
+        <RangeSliderss
+          value={ volumeRange }
+          onChange={ (value)=> sliderOnChange(value) }
+          max={100}
+          min={10}
+          step={ 10 }
+        />
         </Container>
       </div>
     </>
